28ced0b4c9db4125c7f5f2d05c8dcfe8f263e641
Piotr <piotr.samborski007@gmail.com>
2021-05-08 22:59:15 +0200

    delete comments

--- ToeplitzMatrixMultiplication/ToeplitzMatrixMultiplication/ToeplitzMultiplication.cs
+++ ToeplitzMatrixMultiplication/ToeplitzMatrixMultiplication/ToeplitzMultiplication.cs
@@ -143,14 +143,8 @@ namespace ToeplitzMatrixMultiplication
         public static Complex[] fft(Complex[] x)
         {
             int N = x.Length;
-
-            // base case
             if (N == 1) return new Complex[] { x[0] };
-
-            // radix 2 Cooley-Tukey FFT
             if (N % 2 != 0) { throw new Exception("N is not a power of 2"); }
-
-            // fft of even terms
             Complex[] even = new Complex[N / 2];
             for (int k = 0; k < N / 2; k++)
             {
@@ -158,15 +152,13 @@ namespace ToeplitzMatrixMultiplication
             }
             Complex[] q = fft(even);
 
-            // fft of odd terms
-            Complex[] odd = even;  // reuse the array
+            Complex[] odd = even;
             for (int k = 0; k < N / 2; k++)
             {
                 odd[k] = x[2 * k + 1];
             }
             Complex[] r = fft(odd);
 
-            // combine
             Complex[] y = new Complex[N];
             for (int k = 0; k < N / 2; k++)
             {
@@ -179,25 +171,18 @@ namespace ToeplitzMatrixMultiplication
         }
 
 
-        // compute the inverse FFT of x[], assuming its length is a power of 2
         public static Complex[] ifft(Complex[] x)
         {
             int N = x.Length;
             Complex[] y = new Complex[N];
 
 
-            // take conjugate
             for (int i = 0; i < N; i++)
             {
                 y[i] = x[i].conjugate();
             }
 
-
-            // compute forward FFT
             y = fft(y);
-
-
-            // take conjugate again
             for (int i = 0; i < N; i++)
             {
                 y[i] = (y[i].conjugate()).times(1.0 / N);
